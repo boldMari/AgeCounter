84ddbd9976877933fc1d47de67c26ca8033ec227
boldMari <marianna@kubice.net>
2023-05-17 14:46:34 +0200

    another switch to different approach

--- age-counter.js
+++ age-counter.js
@@ -2,35 +2,55 @@ function calculateAge() {
   var birthdate = new Date(document.getElementById("birthdate").value);
   var now = new Date();
 
-  var ageYears = now.getFullYear() - birthdate.getFullYear();
-  var ageMonths = now.getMonth() - birthdate.getMonth();
-  var ageDays = now.getDate() - birthdate.getDate();
+  var years = now.getFullYear() - birthdate.getFullYear();
+  var months = now.getMonth() - birthdate.getMonth();
+  var days = now.getDate() - birthdate.getDate();
 
   // Check if the birth month and day have passed in the current year
-  if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
-    ageYears--;
-    ageMonths += 12;
+  if (months < 0 || (months === 0 && now.getDate() < birthdate.getDate())) {
+    years--;
+    months = 12 - birthdate.getMonth() + now.getMonth();
   }
-  
-  if (days < 0 || (days === 0 && today.getHours() < birthDate.getHours())) {
-        months--;
-        days = days + daysInMonth(today.getMonth(),today.getFullYear()); 
+
+  // Adjust months and years if the current day is before the birth day
+  if (now.getDate() < birthdate.getDate()) {
+    months--;
+    if (months < 0) {
+      months = 11;
+      years--;
     }
+    var tempDate = new Date(now.getFullYear(), now.getMonth() - 1, birthdate.getDate());
+    var diff = Math.floor((now - tempDate) / (1000 * 60 * 60 * 24));
+    days = diff >= 0 ? diff : 0;
+  }
 
-
-  // Adjust the number of days for varying month lengths
-  var birthdateThisYear = new Date(now.getFullYear(), birthdate.getMonth(), birthdate.getDate());
-  var daysDifference = Math.floor((now - birthdateThisYear) / (1000 * 60 * 60 * 24));
-  var totalDays = daysDifference - (ageMonths * 30) - (ageYears * 365);
+  // Calculate the age in days by considering varying month lengths
+  var ageInDays = calculateDays(birthdate, now);
 
   // Display the age in the result div
   document.getElementById("result").innerHTML =
     "Your age is " +
-    ageYears +
+    years +
     " years, " +
-    ageMonths +
+    months +
     " months, and " +
-    totalDays +
-    " days.";
+    days +
+    " days. (Approximately " +
+    ageInDays +
+    " days).";
 }
+
+function calculateDays(start, end) {
+  var count = 0;
+  var current = new Date(start);
+
+  while (current <= end) {
+    count++;
+    current.setDate(current.getDate() + 1);
+  }
+
+  return count - 1; // Subtracting 1 to exclude the end date
+}
+
+
 
